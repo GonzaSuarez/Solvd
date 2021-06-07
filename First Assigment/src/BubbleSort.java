c7ec7a8e7f8fb9a5410d5055c58302ece43d1b39
Gonzalo Suarez <legonza424@gmail.com>
2021-06-07 11:37:37 -0300

    m

--- First Assigment/src/BubbleSort.java
+++ First Assigment/src/BubbleSort.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BubleSort {
+public class BubbleSort {
+
+    public void sort(List<Integer> myList){
+        Integer temp = 0;
+        for(int i = 0; i<myList.size(); i++){
+            for(int j = 1; j<myList.size(); j++){
+                if(myList.get(j-1).intValue() > myList.get(j).intValue()){
+                    temp = myList.get(j-1);
+                    myList.set(j-1, myList.get(j));
+                    myList.set(j,temp);
+                }
+            }
+        }
+    }
+
+
 }
--- First Assigment/src/Main.java
+++ First Assigment/src/Main.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    public static void print(List<Integer> my_list){
+        for (Integer i: my_list){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+    public static final void main(String[] args){
+        List<Integer> to_sort_list = new ArrayList<>();
+
+        for(int i=0; i< args.length; i++){
+            try{
+                to_sort_list.add(Integer.parseInt(args[i]));
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("NumberFormatException: " + nfe.getMessage());
+            }
+        }
+
+        BubbleSort bubbleSort = new BubbleSort();
+        System.out.println("Unsorted List");
+        print(to_sort_list);
+        bubbleSort.sort(to_sort_list);
+        System.out.println("Sorted List");
+        print(to_sort_list);
+
+    }
+
 }
